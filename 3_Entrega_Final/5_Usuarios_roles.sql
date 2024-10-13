4e4822ed0dbb29b8531eee6d2dbca3064379fef7
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2024-10-13 18:37:50 -0500

    creando 2 usuarios

--- 3_Entrega_Final/5_Usuarios_roles.sql
+++ 3_Entrega_Final/5_Usuarios_roles.sql
@@ -1,4 +1,10 @@
 -- ######################################## USERS ##############################################
--- ========= USER 1 ===============
+SELECT Host, User FROM mysql.user; -- Muestra todos los usuarios;
 SELECT USER(); -- Ver el usuario actual
+-- ========= USER 1 ===============
+CREATE USER 'coderhouse'@'%'
+IDENTIFIED BY 'coderhouse';
 -- ========= USER 2 ===============
+CREATE USER 'coderhouse_alumno'@'%'
+IDENTIFIED BY 'coderhouse'
+COMMENT 'ESTE USUARIO SOLO VA ACCEDER POR MEDIO DE WORKBENCH'
