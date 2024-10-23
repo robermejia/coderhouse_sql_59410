0cff2c9248c52e81ccd571b605e3dd4476c17652
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2024-10-23 22:56:00 +0000

    Usando automatización

--- 3_Entrega_Final/Extras/runner_mysql_database.sh
+++ 3_Entrega_Final/Extras/runner_mysql_database.sh
@@ -13,18 +13,5 @@ do
 done
 
 echo "Generamos arquitectura base"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/1_Estructura_project.sql"
-echo "Generamos ingesta de datos"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/2_Population.sql"
-echo "Generamos Vistas"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/4_a_Vistas.sql"
-echo "Generamos Funciones"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/4_b_Funciones.sql"
-echo "Generamos Stored procedures"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/4_c_Stored_procedures.sql"
-echo "Generamos Triggers"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/4_d_Triggers.sql"
-echo "Generamos Roles y usuarios"
-docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/5_Roles_Usuarios.sql"
-
+docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/Script_Entrega_Final_Roberto_Mejia_Collazos.sql"
 echo "Carga completa"
