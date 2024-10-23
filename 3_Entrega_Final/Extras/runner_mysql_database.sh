8961b1d252203605c3799ef960bd17091dc0178f
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2024-10-23 23:05:53 +0000

    Actualizando comentarios

--- 3_Entrega_Final/Extras/runner_mysql_database.sh
+++ 3_Entrega_Final/Extras/runner_mysql_database.sh
@@ -13,5 +13,11 @@ do
 done
 
 echo "Generamos arquitectura base"
+## El archivo: Script_Entrega_Final_Roberto_Mejia_Collazos.sql es el archivo final ya para subir a Workbench o carga en Codespace.
 docker exec -it mysql mysql -u root -pcoderhouse -e "source /sql_code/Script_Entrega_Final_Roberto_Mejia_Collazos.sql"
 echo "Carga completa"
+
+## Ejecutando automatización
+## 1. Estar ubicado en la carpeta Extras
+## 2. Ejecutar sh runner_mysql_database.sh
+## 3. En este caso me cargo en el puerto 8080
